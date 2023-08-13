e0d7aa52c0008f7b3aeda1135cae4bff1dcf4a38
Ashwin Raj C K <ashwinraj275@gmail.com>
2023-08-13 19:12:02 +0530

    paypal bug

--- project/src/components/LoadingSpinner/InnerLoader.jsx
+++ project/src/components/LoadingSpinner/InnerLoader.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import "./inner.css";
+import "./Inners.css"
 function InnerLoader() {
   return (
     <div className="fixed top-0 right-0 bottom-0 left-0 flex flex-col justify-center opacity-80  bg-zinc-50   h-screen w-full">
