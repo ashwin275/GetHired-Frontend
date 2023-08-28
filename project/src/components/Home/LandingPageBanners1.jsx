b26a740a1476ec18dafb97905509207c617e6b30
Ashwin Raj C K <ashwinraj275@gmail.com>
2023-08-28 16:19:32 +0530

    image bug

--- project/src/components/Home/LandingPageBanners1.jsx
+++ project/src/components/Home/LandingPageBanners1.jsx
@@ -5,13 +5,13 @@ function LandingPageBanners1() {
     <div>
         <div className=" md:h-[28rem]  mt-5 flex justify-center ">
       <div className="w-full flex flex-col md:flex md:flex-row lg:py-9 md:h-[29rem]  p-2">
-        <div className="w-full md:w-1/2 h-1/2  md:h-full bg-grey-200">
+        <div className="w-full md:w-1/2 h-1/2  md:h-full bg-grey-200 mb-5">
           <div className="w-full flex justify-center">
             <img className=" w-5/5 h-5/5 lg:w-[30rem] lg:h-[30rem] lg:object-cover" src="https://camo.githubusercontent.com/40165a147c3dcea0fa1db780bb533fc5f98546ccfb9d5d05ddb2f429277f5348/68747470733a2f2f616e616c7974696373696e6469616d61672e636f6d2f77702d636f6e74656e742f75706c6f6164732f323031382f31322f646576656c6f7065722d6472696262626c652e676966" alt="Image"></img>
           </div>
         </div>
 
-        <div className="w-full md:w-1/2 px-4 max-h-64  md:py  grid  md:px-20 content-center lg:mt-16">
+        <div className="w-full md:w-1/2 px-4 max-h-64  md:py  grid  md:px-20 content-center lg:mt-40">
           <div className=" lg:w-10/12 mx-auto h-10/12  text-lg md:text-3xl lg:text-[2rem] lg:font-medium font-serif lg:text-start mx-auto text-teal-900">
             <p className="">Optimize Your Hiring Process </p>
             <p className="  lg:mt-3 ">With Increased Efficiency and </p>
--- project/src/components/Home/LandingPageBanners2.jsx
+++ project/src/components/Home/LandingPageBanners2.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 function LandingPageBanners2() {
   return (
     <div>
-        <div className=" md:h-[28rem]   flex justify-center  ">
+        <div className=" md:h-[28rem] mt-5  flex justify-center  ">
       <div className="w-full flex flex-col md:flex md:flex-row lg:py-9  md:h-[29rem]  p-2">
-        <div className="w-full md:w-1/2 h-1/2  md:h-full ">
-          <div className="w-full flex justify-center mb-10 rounded-full">
-            <img className=" w-5/5 h-5/5 lg:w-[30rem] lg:h-[30rem] lg:object-cover rounded-lg" src="https://i.pinimg.com/originals/8e/04/46/8e0446d18d1794620e580d8e2cfcaedb.gif" alt="Image"></img>
+        <div className="w-full md:w-1/2 h-1/2  md:h-full  bg-grey-200 mb-5">
+          <div className="w-full flex justify-center ">
+            <img className=" w-5/5 h-5/5 lg:w-[26rem] lg:h-[26rem] lg:object-cover rounded-lg  " src="https://cdn.dribbble.com/users/1720296/screenshots/4938330/media/c0f9f7a22d2cb33c6b4427269a8e5ae1.gif" alt="Image"></img>
           </div>
         </div>
 
-        <div className="w-full md:w-1/2 px-4 max-h-64  md:py  grid  md:px-20 content-center lg:mt-40">
+        <div className="w-full md:w-1/2 px-4 max-h-64  md:py  grid  md:px-20 content-center lg:mt-24">
           <div className=" lg:w-10/12 mx-auto h-10/12  text-lg mt-10 md:text-3xl lg:text-[2rem] lg:font-medium font-serif lg:text-start mx-auto text-teal-900">
             <p className="">Your Criteria, Your Goals </p>
             <p className="lg:mt-3 ">Get Hired's Job Suggestions </p>
